3fee14715f8fe942c0237c6cdec1c89bd0f95a4d
mashho <mashhooddanish@gmail.com>
2025-02-18 17:50:44 +0530

    Added Manage Staff page

---
+++ frontend/src/pages/AdminPages/AdminHome/ManageStaffModal/manageStaffModal.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import './manageStaffModal.css';
+import CloseIcon from '@mui/icons-material/Close';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+const ManageStaffModal = (props) => {
+
+    const handleSubmitForm = (e)=>{
+        e.preventDefault();
+    }
+    return (
+        <div className='manage-staffs-modal'>
+            <div className='manage-staffs-card'>
+                <div className='add-medicine-header'>
+                    <div>Manage Staffs</div>
+                    <div className='close-modal-report' onClick={() => props.openCloseModal()}><CloseIcon /></div>
+                </div>
+                <div className='add-staffs-box'>
+                    <form className='register-form' onSubmit={handleSubmitForm}>
+                        <div className='register-form-div'>
+                            <div className='register-input-box'>
+                                <input type='text' className='input-box-register' placeholder='Staff Name' />
+                            </div>
+                            <div className='register-input-box'>
+                                <input type='email' className='input-box-register' placeholder='Email Id' />
+                            </div>
+                            <div className='register-input-box'>
+                                <input type='password' className='input-box-register' placeholder='Password' />
+                            </div>
+                            <div className='register-input-box'>
+                                <input type='text' className='input-box-register' placeholder='Designation' />
+                            </div>
+
+                        </div>
+                        <button type='submit' className='form-btn reg-btn'>Add</button>
+
+
+                    </form>
+                </div>
+
+                <div className='list-staffs'>
+                    <div className='list-staff'>
+                        <div>Mr. Sanjay Kumar Rao </div>
+                        <div className='list-staff-btns'>
+                            <div className='edit-icon'><EditIcon/></div>
+                            <div className='delete-icon'><DeleteIcon/></div>
+                        </div>
+                    </div>
+
+                    <div className='list-staff'>
+                        <div>Mr. Sanjay Kumar Rao </div>
+                        <div className='list-staff-btns'>
+                            <div className='edit-icon'><EditIcon/></div>
+                            <div className='delete-icon'><DeleteIcon/></div>
+                        </div>
+                    </div>
+
+                    <div className='list-staff'>
+                        <div>Mr. Sanjay Kumar Rao </div>
+                        <div className='list-staff-btns'>
+                            <div className='edit-icon'><EditIcon/></div>
+                            <div className='delete-icon'><DeleteIcon/></div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ManageStaffModal;
--- frontend/src/pages/AdminPages/AdminHome/adminDashboard.jsx
+++ frontend/src/pages/AdminPages/AdminHome/adminDashboard.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './adminDashboard.css';
 import { Link } from 'react-router-dom';
+import ManageStaffModal from './ManageStaffModal/manageStaffModal';
 const AdminDashboard = () => {
+  const [manageModal,setManageModal] = useState(false);
+  const openCloseModal = ()=>{
+    setManageModal(prev=>!prev)
+  }
   return (
     <div className='adminDashboard'>
         <div className='welcome-header'>
           <div className='welcome-admin'>Welcome To Admin Panel</div>
-          <div className='manage-staff-btn'>Manage Staffs</div>
+          <div className='manage-staff-btn' onClick={openCloseModal}>Manage Staffs</div>
         </div>
         <div className='admin-dashboard-cards'>
           <Link to={'/admin/register-student'} className='admin-dashboard-card'>
@@ -19,6 +24,7 @@ const AdminDashboard = () => {
             Records
           </div>
         </div>
+        {manageModal && <ManageStaffModal openCloseModal={openCloseModal} />}
     </div>
   )
 }
